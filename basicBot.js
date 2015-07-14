c010123b8b244944a4391b7f7331a80ed69ec911
Moondye7 <karottenkopf@arcor.de>
2015-07-14 16:36:22 +0200

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2886,7 +2886,7 @@
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                        API.sendChat(basicBot.chat.softeis)
+                        API.sendChat(basicBot.chat.softeis) {name: chat.un}));
                     }
                 }
             },
