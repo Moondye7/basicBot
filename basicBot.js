66e376882f919d0746abc3bf10b76fd6b0dc233b
Moondye7 <karottenkopf@arcor.de>
2015-07-15 16:06:35 +0200

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -3765,6 +3765,19 @@
                     }
                 }
             },
+            
+            simplexCommand: {
+                command: 'simplex',
+                rank: 'user',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        API.sendChat(basicBot.chat.simplex)
+                    }
+                }
+            },
 
             whoisCommand: {
                 command: 'whois',
