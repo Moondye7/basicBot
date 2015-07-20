babc4f6ea3f46d66d81fea8e74d38540a680c165
Moondye7 <karottenkopf@arcor.de>
2015-07-20 14:22:36 +0200

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -3740,6 +3740,19 @@
                 }
             },
             
+            nextstreamCommand: {
+                command: 'nextstream',
+                rank: 'user',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        API.sendChat(basicBot.chat.nextstream)
+                    }
+                }
+            },
+            
             nazimodCommand: {
                 command: 'nazimod',
                 rank: 'user',
