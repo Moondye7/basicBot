015fda136bef392d04fbc4598c8217d999b9f10e
Daniel <karottenkopf@arcor.de>
2015-08-08 17:53:36 +0200

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -236,7 +236,7 @@
     var botCreatorIDs = ["3851534", "4105209"];
 
     var basicBot = {
-        version: "2.8.10",
+        version: "2.8.11",
         status: false,
         name: "basicBot",
         loggedInID: null,
